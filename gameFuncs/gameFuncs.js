8c622e838604cece1e302cf0daeea0741173cf97
Elwin <elwin.carlos09@gmail.com>
2021-03-04 20:59:19 +0000

    Add basic pet class

---
+++ gameFuncs/gameFuncs.js
@@ -0,0 +1,9 @@
+//Create a pet class
+
+class Pet {
+    constructor (name, hungerLevel=10, happinessLevel=10) {
+        this.name = name;
+        this.hungerLevel = hungerLevel;
+        this.happinessLevel = happinessLevel;
+    }
+}
